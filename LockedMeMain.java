9823b70b8b1231962e34c4e1f9aff65f6055de89
suryabp507 <100464929+suryabp507@users.noreply.github.com>
2022-03-10 20:41:19 +0530

    Add files via upload

--- LockedMeMain.java
+++ LockedMeMain.java
@@ -1,1 +1,13 @@
+package LockedMe.com;
 
+public class LockedMeMain {
+
+	public static void main(String[] args) {
+FileOperations.createMainFolderIfNotPresent("main");
+MenuOptions.printWelcomeScreen("LockedMe", "suhas");
+		
+		HandleOptions.handleWelcomeScreenInput();
+
+	}
+
+}
